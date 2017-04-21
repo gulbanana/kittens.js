19974f1c1036bf544f14bb1a8d836690efe6f01f
Thomas Castiglione <castiglione@mac.com>
2017-04-21 12:36:17 +0800

    improve the top-level strategy by treating blueprints as a demand resource

--- src/main.ts
+++ src/main.ts
@@ -5,80 +5,40 @@ namespace bot {
         /*************************************************************/
         /* 1: build when buildings are affordable, in priority order */
         /*************************************************************/
-        // housing
-        onAffordBuy("logHouse");
-        onAffordBuy("hut");
-        onAffordBuy("mansion");
+        let buildings = [
+            "logHouse", "hut", "mansion", //housing
+            "workshop", "steamworks", "magneto", "factory", "ziggurat", // metaproduction
+            "unicornPasture", "mine", "lumberMill", "smelter", "quarry", "oilWell", "accelerator", "calciner", //production
+            "pasture", "field", "aqueduct", "reactor", //food/energy
+            "academy", "library", "observatory", "biolab", //science
+            "barn", "warehouse", "harbor", //storage
+            "tradepost", "amphitheatre", "temple", "chapel", //culture
+        ];
 
-        // construction
-        onAffordBuy("workshop");
-
-        // production
-        onAffordBuy("unicornPasture");
-        onAffordBuy("mine");
-        onAffordBuy("lumberMill");
-        onAffordBuy("smelter");
-        onAffordBuy("quarry");
-        onAffordBuy("oilWell");
-        onAffordBuy("accelerator");
-
-        // food
-        onAffordBuy("pasture");
-        onAffordBuy("field");
-        onAffordBuy("aqueduct");
-
-        // science
-        onAffordBuy("academy");
-        onAffordBuy("library");
-        onAffordBuy("observatory");
-        onAffordBuy("biolab");
-
-        // culture
-        onAffordBuy("tradepost");
-        onAffordBuy("amphitheatre");
-        onAffordBuy("temple");
-        onAffordBuy("chapel");
-
-        // storage
-        onAffordBuy("barn");
-        onAffordBuy("warehouse");
-        onAffordBuy("harbor");
-
-        // blueprint eaters
-        onAffordBuy("steamworks");
-        onAffordBuy("magneto");
-        onAffordBuy("reactor");
-        onAffordBuy("factory");
-        onAffordBuy("calciner");
-        onAffordBuy("ziggurat");
-
-        if (game.spaceTab.visible) {
-            // starcharts
-            onAffordBuy("researchVessel", "piscine");
-            onAffordBuy("sattelite", "cath");
-
-            // space production
-            onAffordBuy("moonOutpost", "moon");
-            onAffordBuy("planetCracker", "dune");
-            onAffordBuy("hydrofracturer", "dune");
-            onAffordBuy("spaceElevator", "cath");
-            onAffordBuy("spiceRefinery", "dune");
-
-            // space housing (deprioritised because it doesn't speed things up as much at this stage of the game)
-            onAffordBuy("spaceStation", "cath");
-            
-            // space storage
-            onAffordBuy("moonBase", "moon");
+        for (let building of buildings) {
+            onAffordBuy(building);
         }
 
-        /***********************************************/
-        /* 2: consume capped resources in various ways */
-        /***********************************************/
+        if (game.spaceTab.visible) {
+            let spaceBuildings = [
+                ["researchVessel", "piscine"], ["sattelite", "cath"], //starcharts
+                ["moonOutpost", "moon"], ["planetCracker", "dune"], ["hydrofracturer", "dune"], ["spaceElevator", "cath"], ["spiceRefinery", "dune"], //production
+                ["spaceStation", "cath"], ["moonBase", "moon"], //housing and storage
+            ];
+
+            for (let building of spaceBuildings) {
+                onAffordBuy(building[0], building[1]);
+            }
+        }
+
+        /****************************************/
+        /* 2: consume capped resources usefully */
+        /****************************************/
         // trade if we can
         onCap("gold", (c) => game.diplomacy.tradeMultiple(game.diplomacy.races[4], Math.min(price(c, 15), price(game.resPool.resourceMap["manpower"].value, 50))),
-                    () => (game.resPool.resourceMap["iron"].value <= game.resPool.resourceMap["iron"].maxValue * capThresh || 
-                            game.resPool.resourceMap["titanium"].value <= game.resPool.resourceMap["titanium"].maxValue * capThresh) && 
-                            game.resPool.resourceMap["manpower"].value > 50);
+                      () => (game.resPool.resourceMap["iron"].value <= game.resPool.resourceMap["iron"].maxValue * capThresh || 
+                             game.resPool.resourceMap["titanium"].value <= game.resPool.resourceMap["titanium"].maxValue * capThresh) && 
+                             game.resPool.resourceMap["manpower"].value > 50);
 
         // do basic conversion crafting
         onCapCraft("wood", "beam", 175);
@@ -97,15 +57,16 @@ namespace bot {
         onCapCraftOrDemand("culture", "manuscript", "parchment", 400, 25);
         onCapCraftOrDemand("science", "compedium", "manuscript", 10000, 50);
 
-        /*****************************************/
-        /* 3: convert crafted resources up tiers */
-        /*****************************************/
+        /**********************************************************/
+        /* 3: convert crafted resources up tiers to fulfil demand */
+        /**********************************************************/
         onDemandCraft("parchment");
         onDemandCraft("scaffold");
         onDemandCraft("megalith");
         onDemandCraft("gear");
         onDemandCraft("alloy");
         onDemandCraft("concrate");
+        onDemandCraft("blueprint");
     }
 }
 
